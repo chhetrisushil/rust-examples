35235bb4945902b69b17dc80c65688b06e5491ef
Sushil Chhetri <chhetrisushil@gmail.com>
2020-01-04 03:53:21 +0530

    Format fix

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod custom_lib;
 
 fn main() {
-    let arr: [i32; 6] = [10, 23, 30, 40, 42, 50];
-    let len: i32 = arr.len() as i32;
-    let start: i32 = 0;
-    let end: i32 = len - 1;
-    let number_to_find: i32 = 42;
-    println!("number found at: {}", custom_lib::bst::run(number_to_find, &arr, start, end));
+  let arr: [i32; 6] = [10, 23, 30, 40, 42, 50];
+  let len: i32 = arr.len() as i32;
+  let start: i32 = 0;
+  let end: i32 = len - 1;
+  let number_to_find: i32 = 42;
+  println!(
+    "number found at: {}",
+    custom_lib::bst::run(number_to_find, &arr, start, end)
+  );
 }
